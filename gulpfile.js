10f6ae6c39fd9e1521ffa3bcc639e7f9cc98fa19
art-henry <ooohenrik@outlook.com>
2022-11-16 13:05:49 +0100

    Был добавлен gulpfile для отслеживания изменений в файлах и перезагрузки браузера

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,36 @@
-var gulp = require('gulp');
-gulp.task('default', function () {
-    console.log("Hello I'm gulp" )
+const gulp = require('gulp'); // Подключаем Gulp
+const    browserSync  = require('browser-sync'); // Подключаем Browser Sync
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({
+        server: {baseDir: './'},
+        startPath: 'index.html',
+        serveStaticOptions: {extensions: ["html"] },
+        ghostMode: {scroll: false },
+        notify: false,
+      });
+});
+
+gulp.task('css', function () {
+    return gulp.src('css/**/*.css')
+        .pipe(browserSync.reload({stream: true}))
 })
+
+gulp.task('script', function () {
+    return gulp.src('js/**/*.js')
+        .pipe(browserSync.reload({stream: true}))
+})
+
+gulp.task('html', function() {
+    return gulp.src('*.html')
+        .pipe(browserSync.reload({stream: true}));
+})
+
+gulp.task('watch', function() {
+    gulp.watch(['css/**/*.css'], gulp.parallel('css')); // Наблюдение за css файлами в папке css
+    gulp.watch(['js/**/*.js'], gulp.parallel('script')); // Наблюдение за css файлами в папке css
+    gulp.watch('*.html', gulp.parallel('html'));
+});
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync',
+() => { console.log('dev start');}))
